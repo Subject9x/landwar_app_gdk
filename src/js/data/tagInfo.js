e46094fffb61905b659674703f2e39b35617ffea
Subject9x <pwroohr@gmail.com>
2022-01-06 08:36:50 -0500

    tag info work.

--- src/js/data/tagInfo.js
+++ src/js/data/tagInfo.js
@@ -2,15 +2,24 @@
 const tagInfo = {
    data :[ 
         {
-            id : 0,
             title : 'Adv. Gun Sights',
             desc : 'Target of this Unit cannot gain +2 DEF when attacked at Long Range.',
             func : (moveVal, rangeDamageVal) =>{return ((moveVal / 2) + (rangeDamageVal / 2));}
         },
         {
-            id : 1,
             title : 'Afterburner',
-            desc : 'During the Movement Phase, Unit may forego its Attack this turn to move up to double its max Move value.'
+            desc : 'During the Movement Phase, Unit may forego its Attack this turn to move up to double its max Move value.',
+            func : (armorVal, moveVal) => {return ((armorVal/2) + (moveVal/3));}
+        },
+        {
+            title : 'Armor Piercing',
+            desc : 'When applying Damage from this unit’s attack. Apply the damage amount to the Target Model’s structure even if the Target Model has remaining armor points.',
+            func : (rangeDamageVal) => {return (uc_calc_Damage_Range(rangeDamageVal) * 0.75);}
+        },
+        {
+            title : 'Artillery',
+            desc : 'Unit`s IF attack gains a Blast radius of 6"',
+            func : (rangeDamageVal) => {return rangeDamageVal * 2}
         }
    ]
 };
