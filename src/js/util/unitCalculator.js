abcc9d1efb2e3b212b1b578f1b8f38a1c6e78c24
subject9x <pwroohr@gmail.com>
2024-04-24 21:11:36 -0400

    update: CORE - tags - Forward Observer, Crew, Terrifying - reworked cost curves.
    + Unit Calculator - added Size / 3 back as an always paid cost.
    + quickplay - tiny format change.

--- src/js/util/unitCalculator.js
+++ src/js/util/unitCalculator.js
@@ -1,14 +1,18 @@
 /*
     Core Unit cost calculator function.
 
-    Unit Builder Rules v0.6
+    Unit Builder Rules v0.8
 
         Core Stat Equations, TAGS are elsewhere.
 
         these are distinct from the unitbuilder bindings to make them accessible anywhere.
 */
+
+// Beta 1.5 - size is more of a meta stat that affects other calcs, its a but redundant to factor this as a cost unto-itself, but it's a nice 'rounding
+// value for additional cost bulk.
 function uc_calc_Size(sizeVal){
-    return sizeVal * 2;
+    // return sizeVal * 2; // old, pre 1.3
+    return sizeVal / 3; 
 }
 
 function uc_calc_Move(moveVal, sizeVal){
